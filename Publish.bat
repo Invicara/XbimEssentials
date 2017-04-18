e692d1fba7069a713da48f3ef080bfe5f51242bd
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-18 16:18:07 +0800

    Use "Release|Any CPU" configuration to build

--- Publish.bat
+++ Publish.bat
@@ -1,4 +1,9 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /build "Release|x64"
+rm -rf packages
+nuget restore "Xbim.Essentials.Nuget.sln"
+sleep 2
+nuget restore "Xbim.Essentials.Nuget.sln"
+
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /build "Release|Any CPU"
 nuget pack Xbim.Essentials.nuspec
 copy Xbim.*.nupkg ..\LocalPackages
 del Xbim.*.nupkg
