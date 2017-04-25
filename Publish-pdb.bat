a4f55336ef042da03c53c054e4d8db427f4f29f3
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-25 15:59:36 +0800

    Batch file to build Release version with debugable information and an option for different cpu type

--- Publish-pdb.bat
+++ Publish-pdb.bat
@@ -3,7 +3,11 @@ nuget restore "Xbim.Essentials.Nuget.sln"
 sleep 2
 nuget restore "Xbim.Essentials.Nuget.sln"
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /build "ReleaseWithPDB|x64"
+set cputype=%~1
+if "%~1" == "" set cputype=x64
+
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /build "ReleaseWithPDB|%cputype%"
+
 nuget pack Xbim.Essentials.nuspec
 copy Xbim.*.nupkg ..\LocalPackages
 del Xbim.*.nupkg
