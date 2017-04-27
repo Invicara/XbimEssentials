a2c5a5b173b2f0c747cb07b4ef643fc7f5d92e2b
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-27 15:18:11 +0800

    Add clean operation

--- Publish.bat
+++ Publish.bat
@@ -3,7 +3,12 @@ nuget restore "Xbim.Essentials.Nuget.sln"
 sleep 2
 nuget restore "Xbim.Essentials.Nuget.sln"
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /build "Release|x64"
+set buildConfig="%~1"
+if "%~1" == "" set buildConfig="Release|Any CPU"
+
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /clean %buildConfig%
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Essentials.Nuget.sln" /build %buildConfig%
+
 nuget pack Xbim.Essentials.nuspec
 copy Xbim.*.nupkg ..\LocalPackages
 del Xbim.*.nupkg
